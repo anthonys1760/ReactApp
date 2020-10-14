759dd30aef90bf6f445e17dab81dfc5ea2a4c318
Anthony <anthony.smith1760@gmail.com>
2020-10-14 11:07:06 -0400

    Rendered Elements

--- 2 - Add React to a Project/scoreboard/app.js
+++ 2 - Add React to a Project/scoreboard/app.js
@@ -1,20 +1,15 @@
 
-const desc = 'I just learned how to create a React node and render it into the DOM';
-const myTitleID = 'main-title';
-const name = 'Anthony';
-
-
-const header = (
-    <header>
-        <h1 id={myTitleID}>{ name }'s First React Element!</h1>
-        <p className="">{ desc }</p>
-        <input value={10 * 20} />
-    </header>
-);
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
 
 ReactDOM.render(
-    header,
+    <Header />,
     document.getElementById('root')
 );
 
-console.log(title);
