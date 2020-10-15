329444a4b45ea2bace9b4e9e2b9adc405daabcf2
Anthony <anthony.smith1760@gmail.com>
2020-10-15 13:32:26 -0400

    Added additional component attributes and keys

--- 2 - Add React to a Project/scoreboard/app.js
+++ 2 - Add React to a Project/scoreboard/app.js
@@ -1,54 +1,85 @@
 
+const players = [
+    {
+        name: "Guil",
+        score: 50,
+        id: 1
+      },
+      {
+        name: "Treasure",
+        score: 85,
+        id: 3
+      },
+      {
+        name: "Ashley",
+        score: 95,
+        id: 5
+      },
+      {
+        name: "James",
+        score: 80,
+        id: 9
+      }
+];
+
 //Component 1 (This is the scoreboard)(Arrow Functions being used here)
 //Here is your Arrow Function
-const Header = () => {
+const Header = (props) => {
     //This is what you are returning
 return (
     //This is where your HTML goes
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{props.title}</h1>
+            <span className="stats">Players: {props.totalPlayers}</span>
         </header>
 );
 }
 
 //Component 2 (This is the player name category)
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Anthony
+                {props.name}
             </span>
-            <Counter />
+            <Counter score={props.score} />
         </div>
     );
 }
 
 //Component 3
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score">35</span>
+            <span className="counter-score">{props.score}</span>
             <button className="counter-action increment"> + </button>
         </div>
     );
 }
 
-const App = () => {
+const App = (props) => {
     return (
         <div className="scoreboard">
-            <Header />
+            <Header title="My Scoreboard" 
+            totalPlayers={ props.initialPlayers.length} />
 
             {/*Players list */}
-            <Player />
+            {props.initialPlayers.map( player =>
+                <Player
+                    name={player.name} 
+                    score={player.score}
+                    key={player.id}
+                />
+            )}
+            
         </div>
-    )
+    );
 }
 
 //This component is rendering your other components to the DOM
 ReactDOM.render(
-    <App />,
+    <App initialPlayers={players}/>,
     document.getElementById('root')
 );
 
