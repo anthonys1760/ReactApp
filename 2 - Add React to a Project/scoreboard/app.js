aeafc923a9805d79be6c656a53b2aa2fe1187bfb
Anthony <anthony.smith1760@gmail.com>
2020-10-16 12:46:01 -0400

    Completed App

--- 2 - Add React to a Project/scoreboard/app.js
+++ 2 - Add React to a Project/scoreboard/app.js
@@ -1,27 +1,3 @@
-
-const players = [
-    {
-        name: "Guil",
-        score: 50,
-        id: 1
-      },
-      {
-        name: "Treasure",
-        score: 85,
-        id: 3
-      },
-      {
-        name: "Ashley",
-        score: 95,
-        id: 5
-      },
-      {
-        name: "James",
-        score: 80,
-        id: 9
-      }
-];
-
 //Component 1 (This is the scoreboard)(Arrow Functions being used here)
 //Here is your Arrow Function
 const Header = (props) => {
@@ -40,6 +16,7 @@ const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
+            <button className="remove-player" onClick={ () => props.removePlayer(props.id) }>✖</button>
                 {props.name}
             </span>
             <Counter score={props.score} />
@@ -48,38 +25,91 @@ const Player = (props) => {
 }
 
 //Component 3
-const Counter = (props) => {
-    return (
-        <div className="counter">
-            <button className="counter-action decrement"> - </button>
-            <span className="counter-score">{props.score}</span>
-            <button className="counter-action increment"> + </button>
-        </div>
-    );
+class Counter extends React.Component {
+    state = {
+        score: 0
+    };
+
+    incrementScore = () => {
+        this.setState( prevState => {
+            return {
+                score: prevState.score + 1
+            };
+        });
+    }
+
+    decrementScore = () => {
+        this.setState( prevState => {
+            return {
+                score: prevState.score - 1
+            };
+        });
+    }
+    render() {
+        return (
+            <div className="counter">
+                <button className="counter-action decrement" onClick={() => this.decrementScore()}> - </button>
+                <span className="counter-score">{this.state.score}</span>
+                <button className="counter-action increment" onClick={() => this.incrementScore()}> + </button>
+            </div>
+        );
+    }
 }
 
-const App = (props) => {
-    return (
-        <div className="scoreboard">
-            <Header title="My Scoreboard" 
-            totalPlayers={ props.initialPlayers.length} />
+class App extends React.Component {
 
-            {/*Players list */}
-            {props.initialPlayers.map( player =>
-                <Player
-                    name={player.name} 
-                    score={player.score}
-                    key={player.id}
-                />
-            )}
-            
-        </div>
-    );
+    state = {
+        players: [
+            {
+                name: "Guil",
+                id: 1
+              },
+              {
+                name: "Treasure",
+                id: 3
+              },
+              {
+                name: "Ashley",
+                id: 5
+              },
+              {
+                name: "James",
+                id: 9
+              }
+        ]
+    };
+
+    handleRemovePlayer = (id) => {
+       this.setState( prevState => {
+           return {
+              players: prevState.players.filter( p => p.id !== id )
+           };
+       });
+    }
+
+    render() {
+        return (
+            <div className="scoreboard">
+                <Header title="My Scoreboard" 
+                totalPlayers={ this.state.players.length } />
+    
+                {/*Players list */}
+                {this.state.players.map( player =>
+                    <Player
+                        name={player.name} 
+                        id={player.id}
+                        key={player.id.toString()}
+                        removePlayer={this.handleRemovePlayer}
+                    />
+                )}
+            </div>
+        );
+    }
 }
 
 //This component is rendering your other components to the DOM
 ReactDOM.render(
-    <App initialPlayers={players}/>,
+    <App />,
     document.getElementById('root')
 );
 
